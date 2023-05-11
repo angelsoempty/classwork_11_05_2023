9f6f0d466511c1dc7f36e660275b2f6c8f1773ba
angelsoempty <denatamanov1@gmail.com>
2023-05-11 23:17:40 +0300

    labwork

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Animal:
+"""class Animal:
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -32,3 +32,33 @@ student2 = Student('Sanya', 20, '213512t124')
 with open('info_student.txt', 'w') as file:
     file.write(str(student1))
     file.write(str(student2))
+"""
+
+import random
+class Animal:
+    def __init__(self, species, name, age, health, hunger, happiness)
+        super().__init__()
+        self.species = species
+        self.name = name
+        self.age = age
+        self.health = health
+        self.hunger = hunger
+        self.happiness = happiness
+    def grow(self):
+        self.age += 1
+        self.health = random.randint(0, 10)
+        self.hunger = random.randint(0, 10)
+        self.happiness = random.randint(0, 10)
+    def eat(self):
+        if self.hunger >= 6:
+            self.health += random.randint(0, 5)
+            self.happiness += random.randint(0, 5)
+            self.hunger -= random.randint(5, 7)
+        else:
+            print(self.name, 'голодний')
+    def play(self):
+        pass
+class Zoo:
+    def __init__(self):
+        super().__init__()
+        self.animals = []
