05b5d25a7a93ad69cd8270233236ed4618c3824f
angelsoempty <denatamanov1@gmail.com>
2023-05-16 21:04:01 +0300

    labwork

--- main.py
+++ main.py
@@ -35,50 +35,90 @@ with open('info_student.txt', 'w') as file:
 """
 
 import random
+
 class Animal:
-    def __init__(self, species, name, age, health, hunger, happiness)
-        super().__init__()
+    def __init__(self, species, name, age):
         self.species = species
         self.name = name
         self.age = age
-        self.health = health
-        self.hunger = hunger
-        self.happiness = happiness
+        self.health = 100
+        self.hunger = 0
+        self.happiness = 100
+
     def grow(self):
         self.age += 1
-        self.health = random.randint(0, 10)
-        self.hunger = random.randint(0, 10)
-        self.happiness = random.randint(0, 10)
+        self.health -= random.randint(1, 5)
+        self.hunger += random.randint(1, 5)
+        self.happiness -= random.randint(1, 5)
+
     def eat(self):
-        if self.hunger >= 6:
-            self.health += random.randint(0, 5)
-            self.happiness += random.randint(0, 5)
-            self.hunger -= random.randint(5, 7)
+        if self.hunger > 0:
+            self.hunger -= random.randint(1, 5)
+            self.happiness += random.randint(1, 3)
+            print(f"{self.name} їсть")
         else:
-            print(self.name, 'голодний')
+            print(f"{self.name} не голодний")
+
     def play(self):
-        if self.happiness >= 5:
-            self.health += random.randint(0, 5)
-            self.hunger += random.randint(0, 5)
-            self.happiness += random.randint(0, 5)
+        if self.happiness > 0:
+            self.happiness += random.randint(1, 5)
+            self.hunger += random.randint(1, 3)
+            print(f"{self.name} грається")
         else:
-            print(self.name, 'does not want to play')
+            print(f"{self.name} не хоче гратись")
+
     def __str__(self):
-        return f'{self.species} - {self.name} ({self.age} years old)\n' \
-               f'Health: {self.health}\nHunger: {self.hunger}\nHappiness: {self.happiness}\n'
+        return f"{self.name} це {self.species} і йому {self.age} років"
 
 class Zoo:
     def __init__(self):
-        super().__init__()
         self.animals = []
 
-#Simulating 10 day
-for i in range(1, 11):
-    print(f'Day {i}')
-    save_zoo_state(zoo, i)
+    def add_animal(self, animal):
+        self.animals.append(animal)
+        print(f"{animal.name} додали до зоопарку")
+
+    def remove_animal(self, animal):
+        if animal in self.animals:
+            self.animals.remove(animal)
+            print(f"{animal.name} вигнали з зоопарку")
+        else:
+            print(f"{animal.name} не в зоопарку")
+
+    def feed_all(self):
+        for animal in self.animals:
+            animal.eat()
+
+    def play_with(self):
+        for animal in self.animals:
+            animal.play()
+
+    def grow_all(self):
+        for animal in self.animals:
+            animal.grow()
+
+    def save_zoo_state(self, day):
+        filename = f"Day_{day}.txt"
+        with open(filename, "w") as file:
+            file.write("Статистика:\n")
+            for animal in self.animals:
+                file.write(str(animal) + "\n")
+
+zoo = Zoo()
+
+lion = Animal("Лев", "Бобік", 5)
+elephant = Animal("Слон", "Арчі", 3)
+giraffe = Animal("Жираф", "Лео", 2)
+
+zoo.add_animal(lion)
+zoo.add_animal(elephant)
+zoo.add_animal(giraffe)
+
+for day in range(1, 11):
+    print(f"Day {day}:")
     zoo.feed_all()
-    zoo.play_with(zoo.animals[random.randint(0, len(zoo.animals) - 1)])
-    zoo.geow_all()
-    print(zoo)
-    print('\n')
-print('Simulation finished.')
+    zoo.play_with()
+    zoo.grow_all()
+    zoo.save_zoo_state(day)
+    print()
+
